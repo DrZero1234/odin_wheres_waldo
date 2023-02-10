033ff0bbe7bbb0151f504a1198a9e6c525158720
M.Kristóf <drzero123456@gmail.com>
2023-02-10 20:29:12 +0100

    init

---
+++ src/firebaseConfig.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyCOrM2cMg1j9Ps3-JusJABL7nnBA88ZGEU",
+
+  authDomain: "wheres-waldo-25db2.firebaseapp.com",
+
+  projectId: "wheres-waldo-25db2",
+
+  storageBucket: "wheres-waldo-25db2.appspot.com",
+
+  messagingSenderId: "347727095636",
+
+  appId: "1:347727095636:web:1656dd6aa40d6b0f480ac1"
+
+};
+
+
+// Initialize Firebase
